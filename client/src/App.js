c973f904dd451fac443e5367e980f1cccee0adcc
kanchan <kanchankotkar001@gmail.com>
2022-12-20 15:38:04 +0530

    added useState

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
-function App (){
+function App() {
 
-async function loadData(){
- const response=  await axios.get('/food-items-by-category?category=breakfast')
-  console.log(response.data.data);
+  const [foodItems, setFoodItems] = useState([])
+
+  async function loadData() {
+    const response = await axios.get('/food-items-by-category?category=breakfast')
+    console.log(response.data);
+    setFoodItems(response.data.data)
+  }
+
+
+  return (
+    <>
+      <h1>Food API</h1>
+      <button onClick={loadData}>Make Api call </button>
+      {
+        foodItems.map((item) => {
+          return (<h1>{item.title}</h1>)
+        })
+      }
+
+    </>
+  )
 }
+export default App;
 
 
-return(
-  <>
-  <h1>Food Api</h1>
-   <button onClick = {loadData}>Make Api call </button>
-  </>
-)
-}
-  export default App
-
-  
